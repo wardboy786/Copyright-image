9c04129bb83b886f716e5719521a9013f1c6851c
Umesh Dafda <umeshdafda52@gmail.com>
2025-09-24 17:32:56 +0000

    Does this make new splash screen like we already have one in our webapp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -21,28 +21,11 @@ const config: CapacitorConfig = {
     cleartext: true, // Allow http for local development.
   },
   plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      backgroundColor: "#ffffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
-      splashFullScreen: true,
-      splashImmersive: true,
-      layoutName: "launch_screen",
-      useDialog: true,
-    },
     CapacitorAssets: {
       icon: {
         path: 'public/images/logo.svg'
       },
-      splash: {
-        path: 'public/images/logo.svg'
-      }
+      splash: false // Explicitly disable splash screen generation
     }
   },
 };
