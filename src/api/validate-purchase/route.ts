bcc66045ec741bacf89bdf9559bc30d599b9eec8
Umesh Dafda <umeshdafda52@gmail.com>
2025-09-28 16:12:40 +0000

    Revert back to variables. See Photorights AI Logo Photorights AI 🔍 Sub

--- src/api/validate-purchase/route.ts
+++ src/api/validate-purchase/route.ts
@@ -24,6 +24,10 @@ export async function POST(req: Request) {
   }
       
   const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
     scopes: ['https://www.googleapis.com/auth/androidpublisher'],
   });
 
